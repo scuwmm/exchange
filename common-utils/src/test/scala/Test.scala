24c034119bb1b37c4e92a5abef39973b77cc1abe
scuwmm <scumingming@163.com>
2022-05-23 16:05:33 +0800

    init

--- common-utils/src/test/scala/Test.scala
+++ common-utils/src/test/scala/Test.scala
@@ -1,7 +1,5 @@
 import akka.util.Timeout
-import io.circe.syntax._
 import io.circe.generic.auto._
-import io.circe.parser._
 import scala.concurrent.duration._
 
 object Test extends JsonUtil {
@@ -11,7 +9,7 @@ object Test extends JsonUtil {
   case class People(id: Long, name:String)
   def main(args: Array[String]): Unit = {
     val p = People(1, "wmm")
-    val json = p.asJson.noSpaces
+    val json = encode0(p)
     println(json)
     val p2 = decode0[People](json)
     println(p2)
