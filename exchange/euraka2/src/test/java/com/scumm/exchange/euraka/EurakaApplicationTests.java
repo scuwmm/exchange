238efa809da2a9696bcee138c3f495c0d10663f4
scuwmm <scumingming@163.com>
2022-06-16 14:48:11 +0800

    add eureka cluster2

---
+++ exchange/euraka2/src/test/java/com/scumm/exchange/euraka/EurakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scumm.exchange.euraka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
