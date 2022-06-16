817fc4e6084f567a5fe64d38eefb2efd4a82f4fb
scuwmm <scumingming@163.com>
2022-06-16 10:17:24 +0800

    init

---
+++ spring_eureka/src/main/java/com/sucmm/eureka/SpringEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.sucmm.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaApplication.class, args);
+	}
+
+}
